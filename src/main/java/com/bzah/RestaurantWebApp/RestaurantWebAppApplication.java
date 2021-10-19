098053f6b26585bf1c01e8c02badbc665b56810a
Bilal <44588480+BZahory@users.noreply.github.com>
2021-10-19 03:09:10 -0400

    Initial commit

---
+++ src/main/java/com/bzah/RestaurantWebApp/RestaurantWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.bzah.RestaurantWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bzah/RestaurantWebApp/RestaurantWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bzah.RestaurantWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
